e422c4c50f7969000b37eb85e9a6c24611623fd6
Jesus Ocanto <yisusocanto1984@gmail.com>
2025-11-10 16:41:47 -0400

    fix: CORS config

--- pixel-reviews/pixel-reviews-backend/app/routes/__init__.py
+++ pixel-reviews/pixel-reviews-backend/app/routes/__init__.py
@@ -11,32 +11,41 @@ from app.extensions.marshmallow import ma
 
 load_dotenv()
 
+
 def create_app():
 
-    template_dir = os.path.abspath('templates')
-    static_dir = os.path.abspath('templates/images')
-    
-    app = Flask(__name__, 
-                template_folder=template_dir,
-                static_folder=static_dir)
-    
+    template_dir = os.path.abspath("templates")
+    static_dir = os.path.abspath("templates/images")
+
+    app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
+
+    frontend_url = os.getenv("FRONTEND_URL")
+
+    if frontend_url:
+        allowed_origins = [frontend_url]
+    else:
+        allowed_origins = ["http://localhost:5173", "http://127.0.0.1:5173"]
+
     ma.init_app(app)
-    
-    CORS(app, 
-    resources={r"/*": {
-        "origins": os.getenv("FRONTEND_URL"),
-        "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-        "allow_headers": ["Content-Type", "Authorization"],
-        "expose_headers": ["Set-Cookie"],
-        "supports_credentials": True,
-        "send_wildcard": False
-    }}
-)
-    
+
+    CORS(
+        app,
+        resources={
+            r"/*": {
+                "origins": allowed_origins,
+                "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+                "allow_headers": ["Content-Type", "Authorization"],
+                "expose_headers": ["Set-Cookie"],
+                "supports_credentials": True,
+                "send_wildcard": False,
+            }
+        },
+    )
+
     app.register_blueprint(auth_bp)
     app.register_blueprint(users_bp)
     app.register_blueprint(main_bp)
     app.register_blueprint(api_bp)
     app.register_blueprint(settings_bp)
-    
+
     return app
