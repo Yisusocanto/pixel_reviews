726c4092392c6fa38fd92ed9050919ffd1b712cc
Jesus Ocanto <yisusocanto1984@gmail.com>
2025-11-24 15:44:46 -0400

    Improvement: A context menu has been added to the user profile's wish list.

--- pixel-reviews/pixel-reviews-frontend/src/components/userComponents/WishlistItemCard.tsx
+++ pixel-reviews/pixel-reviews-frontend/src/components/userComponents/WishlistItemCard.tsx
@@ -1,16 +1,21 @@
 import { useAuth } from "@/context/AuthContextProvider";
 // Components
 import { Card } from "../luxe/card";
-import { Calendar, Star, Heart } from "lucide-react";
+import { Calendar, Star, Trash, EllipsisVertical } from "lucide-react";
 import { Link } from "react-router-dom";
 import { Toaster, toast } from "sonner";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 // Services
 import { useRemoveFromWishlist } from "@/hooks/fetching/wishlist/useWishlist";
 // Types
 import type { WishlistItem } from "@/types/wishlistType";
 // Utils
 import { dateFormatter } from "@/utils/dateFormatter";
-import { Button } from "../ui/button";
 
 interface WishlistItemCardProps {
   wishlistItem: WishlistItem;
@@ -33,6 +38,9 @@ function WishlistItemCard({ wishlistItem, ownProfile }: WishlistItemCardProps) {
   const handleRemoveFromWishlist = async () => {
     if (user) {
       removeFromWishlist(wishlistItem.wishlistItemId, {
+        onSuccess: () => {
+          toast.success("Game removed from wishlist");
+        },
         onError: (error) => {
           displayErrorToast(
             (error as any).response.data.error ?? "Unknown error"
@@ -52,36 +60,51 @@ function WishlistItemCard({ wishlistItem, ownProfile }: WishlistItemCardProps) {
             className="rounded-lg shadow-2xl object-cover w-full h-48 md:w-72 md:h-48"
           />
         </div>
-        <div className="flex flex-4 flex-row">
-          <div className="flex flex-4 flex-col gap-4">
-            <h3 className="text-2xl text-primary font-orbitron font-bold ">
-              <Link
-                to={`/games/${wishlistItem.game.slug}`}
-                className="hover:text-primary-muted"
-              >
-                {wishlistItem.game.title}
-              </Link>
-            </h3>
-            <div className="flex gap-2 items-center">
-              <Star size={16} className="text-yellow-300" />
-              <span className="text-sm sm:text-base">
-                Average Rating: {wishlistItem.game.averageRating}/5
-              </span>
-            </div>
-            <div className="flex gap-2 items-center">
-              <Calendar size={16} />
-              <span className="text-sm sm:text-base">
-                Added at {dateFormatter(wishlistItem.addedAt)}
-              </span>
+        <div className="flex w-full">
+          <div className="flex flex-4 flex-row">
+            <div className="flex flex-4 flex-col gap-4">
+              <h3 className="text-2xl text-primary font-orbitron font-bold ">
+                <Link
+                  to={`/games/${wishlistItem.game.slug}`}
+                  className="hover:text-primary-muted"
+                >
+                  {wishlistItem.game.title}
+                </Link>
+              </h3>
+              <div className="flex gap-2 items-center">
+                <Star size={16} className="text-yellow-300" />
+                <span className="text-sm sm:text-base">
+                  Average Rating: {wishlistItem.game.averageRating}/5
+                </span>
+              </div>
+              <div className="flex gap-2 items-center">
+                <Calendar size={16} />
+                <span className="text-sm sm:text-base">
+                  Added at {dateFormatter(wishlistItem.addedAt)}
+                </span>
+              </div>
             </div>
           </div>
-          {ownProfile && (
-            <div className="flex flex-1 justify-end">
-              <Button variant="ghost" onClick={handleRemoveFromWishlist}>
-                <Heart className="text-destructive fill-destructive size-6" />
-              </Button>
-            </div>
-          )}
+          <div>
+            {ownProfile && (
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <EllipsisVertical />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="bg-background">
+                  <DropdownMenuItem
+                    className="focus:bg-(--accent-color)"
+                    onClick={handleRemoveFromWishlist}
+                  >
+                    <Trash size={16} className="text-destructive" />
+                    <span className="text-destructive text-base font-semibold">
+                      Remove from wishlist
+                    </span>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            )}
+          </div>
         </div>
       </Card>
     </div>
